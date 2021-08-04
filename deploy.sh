8bb5d6b69cdb441e390342124d843225726f7a15
LAPTOP-B2807I28\fanwenbin <476767207@qq.com>
2021-08-04 10:16:46 +0800

    修改配置文件

--- deploy.sh
+++ deploy.sh
@@ -12,19 +12,19 @@ cd docs/.vuepress/dist
 # deploy to github pages
 # echo 'b.xugaoyi.com' > CNAME
 
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:fanwenbin123/vuepress-theme-vdoing-blog.git
-else
-  msg='来自github actions的自动部署'
-  githubUrl=https://fanwenbin123:${GITHUB_TOKEN}@github.com/fanwenbin123/vuepress-theme-vdoing-blog.git
-  git config --global user.name "fanwenbin123"
-  git config --global user.email "476767207@qq.com"
-fi
+# if [ -z "$GITHUB_TOKEN" ]; then
+#   msg='deploy'
+#   githubUrl=git@github.com:fanwenbin123/vuepress-theme-vdoing-blog.git
+# else
+#   msg='来自github actions的自动部署'
+#   githubUrl=https://fanwenbin123:${GITHUB_TOKEN}@github.com/fanwenbin123/vuepress-theme-vdoing-blog.git
+#   git config --global user.name "fanwenbin123"
+#   git config --global user.email "476767207@qq.com"
+# fi
 git init
 git add -A
-git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+git commit -m "deploy"
+git push -f git@github.com:fanwenbin123/vuepress-theme-vdoing-blog.git master:gh-pages # 推送到github gh-pages分支
 
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
